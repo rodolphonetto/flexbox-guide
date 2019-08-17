e9925f386c6db480ed04356c4dc703947cc22ce3
Rodolpho Netto <rodolpho.netto@gmail.com>
2019-08-16 22:05:17 -0300

    Area de aling-items

--- js/src/abreVideo.js
+++ js/src/abreVideo.js
@@ -1,15 +1,26 @@
-const videos = document.querySelectorAll(".topico__video");
+const containerVideo = document.querySelectorAll(".topico__video");
 const btnAbreVideo = document.querySelectorAll("#btnVideo");
 
-const aberturaVideo = videoSelecionado => {
-  videos.forEach(video => {
-    video.classList.add("topico__video--ativo");
+const aberturaVideo = buttonData => {
+  containerVideo.forEach(video => {
+    const playerData = video.getAttribute("data-video");
+
+    if (playerData === buttonData) {
+
+      if (video.classList.contains("topico__video--ativo")) {
+        video.classList.remove("topico__video--ativo");
+        video.children[0].src = ""
+      } else {
+        video.children[0].src = playerData
+        video.classList.add("topico__video--ativo");
+      }
+    }
   });
 };
 
 btnAbreVideo.forEach(button => {
-  const videoSelecionado = button.getAttribute("data-video");
+  const buttonData = button.getAttribute("data-video");
   button.addEventListener("click", () => {
-    aberturaVideo(videoSelecionado);
+    aberturaVideo(buttonData);
   });
 });
