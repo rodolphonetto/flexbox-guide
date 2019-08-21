eb8f54038f53280dc8f968465311db309fbd1f86
Rodolpho Netto <rodolpho.netto@gmail.com>
2019-08-21 08:38:52 -0300

    scroll suave funcionando

--- js/src/scrollSuave.js
+++ js/src/scrollSuave.js
@@ -1,19 +1,26 @@
 const links = document.querySelectorAll("#interno");
-const conteudos = document.querySelectorAll(".topico")
+const conteudos = document.querySelectorAll(".topico");
 
-
-const scrollTo = (target) => {
+// Função recebe o alvo, faz um loop entre todas as divs que podem ser o alvo
+// ao encontrar o alvo (pelo data-link) seleciona o top, subtrai 20 (pra dar um respiro na tela)
+// e rola suavemente até lá.
+const scrollTo = target => {
   conteudos.forEach(conteudo => {
     const topicoAlvo = conteudo.getAttribute("data-link");
     if (topicoAlvo === target) {
-      conteudo.scrollIntoView({ behavior: "smooth", block: "start" });
+      const cordenadaY = conteudo.offsetTop - 20;
+      window.scroll({
+        top: cordenadaY,
+        behavior: "smooth"
+      });
     }
-  })
-}
+  });
+};
 
-links.forEach(link => {
+// Cada link recebe um listener de click e passa o seu "alvo" para a função que de fato rola até esse alvo
+links.forEach(link => { 
   const target = link.getAttribute("data-link");
   link.addEventListener("click", () => {
-    scrollTo(target)
-  })
-})
+    scrollTo(target);
+  });
+});
