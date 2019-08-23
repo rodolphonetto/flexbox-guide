a726499d87a70620b1573fd88ef8ec6205edf826
Rodolpho Netto <rodolpho.netto@gmail.com>
2019-08-22 21:46:17 -0300

    iniciado criação dos flex-items

--- js/src/scrollSuave.js
+++ js/src/scrollSuave.js
@@ -1,9 +1,6 @@
 const links = document.querySelectorAll("#interno");
 const conteudos = document.querySelectorAll(".topico");
 
-// Função recebe o alvo, faz um loop entre todas as divs que podem ser o alvo
-// ao encontrar o alvo (pelo data-link) seleciona o top, subtrai 20 (pra dar um respiro na tela)
-// e rola suavemente até lá.
 const scrollTo = target => {
   conteudos.forEach(conteudo => {
     const topicoAlvo = conteudo.getAttribute("data-link");
@@ -17,7 +14,7 @@ const scrollTo = target => {
   });
 };
 
-// Cada link recebe um listener de click e passa o seu "alvo" para a função que de fato rola até esse alvo
+
 links.forEach(link => { 
   const target = link.getAttribute("data-link");
   link.addEventListener("click", () => {
